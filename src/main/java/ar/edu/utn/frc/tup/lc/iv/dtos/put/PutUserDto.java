15ac25c40f159336510b7480f96d304184ba1900
405153-Masera <405153@tecnicatura.frc.utn.edu.ar>
2024-10-07 23:10:41 -0300

    Validaciones para el Put

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/put/PutUserDto.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/dtos/put/PutUserDto.java
@@ -1,5 +1,8 @@
 package ar.edu.utn.frc.tup.lc.iv.dtos.put;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +14,34 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PutUserDto {
+
+    @NotNull(message = "El id no puede ser nulo")
     private Integer id;
+
+    @NotNull(message = "El nombre no puede ser nulo")
+    @Size(min = 1, max = 50, message = "El nombre debe tener entre 1 y 50 caracteres")
     private String name;
+
+    @NotNull(message = "El apellido no puede ser nulo")
+    @Size(min = 1, max = 50, message = "El apellido debe tener entre 1 y 50 caracteres")
     private String lastName;
+
+    @NotNull(message = "El DNI no puede ser nulo")
+    @Size(min = 1, max = 11, message = "El DNI debe ser valido")
     private String dni;
+
+    @NotNull(message = "El contacto no puede ser nulo")
     private Integer contact_id;
+
+    @NotNull(message = "El correo electrónico no puede ser nulo")
+    @Email(message = "El correo electrónico debe ser válido")
     private String email;
+
     private String avatar_url;
+
+    @NotNull(message = "La fecha de nacimiento no puede ser nula")
     private LocalDate datebirth;
+
+    @NotNull(message = "Los roles no pueden ser nulos")
     private List<Integer> userRoles;
 }
