fd2e53b5aa413a6781b3d0b725fb5a325ff350cd
CamilaLopez <113983@tecnicatura.frc.utn.edu.ar>
2024-10-12 20:48:21 -0300

    Test
    Tests de UserController

--- src/test/java/ar/edu/utn/frc/tup/lc/iv/controllers/UserControllerTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/controllers/UserControllerTest.java
@@ -2,6 +2,7 @@ package ar.edu.utn.frc.tup.lc.iv.controllers;
 
 import ar.edu.utn.frc.tup.lc.iv.dtos.get.GetUserDto;
 import ar.edu.utn.frc.tup.lc.iv.dtos.post.PostUserDto;
+import ar.edu.utn.frc.tup.lc.iv.dtos.put.PutUserDto;
 import ar.edu.utn.frc.tup.lc.iv.services.Implementation.UserServiceImpl;
 import ar.edu.utn.frc.tup.lc.iv.services.Interfaces.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,17 +26,18 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.isNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(UserController.class)
@@ -51,34 +53,27 @@ class UserControllerTest {
 
     @Autowired
     private ObjectMapper objectMapperMock;
-//    @Mock
-//    private UserService userService;
-//
-//    @InjectMocks
-//    private UserController userController;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//    @Test
-//    void getUserByEmail() {
-//
-//        String email = "test@example.com";
-//        GetUserDto getUserDto = new GetUserDto();
-//        getUserDto.setEmail(email);
-//
-//        when(userService.getUserByEmail(anyString())).thenReturn(getUserDto);
-//
-//        ResponseEntity<GetUserDto> response = userController.getUserByEmail(email);
-//
-//        assertTrue(response.getStatusCode().is2xxSuccessful());
-//        assertEquals(email, response.getBody().getEmail());
-//    }
 
     @Test
-    void getUsers() throws Exception{
+    void getUserByEmailIT() throws Exception{
+        //Given
+        GetUserDto getUserDto = new GetUserDto(1, "Lucía", "Fernanda", "Lucifer", "123456",
+                "lucii@gmail", "1111111", "4523545", true, "", LocalDate.now(), new String[]{"Security"});
+
+        //When
+        Mockito.when(userServiceMock.getUserByEmail("lucii@gmail")).thenReturn(getUserDto);
+
+        //Then
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/" + getUserDto.getEmail())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value("lucii@gmail"));
+
+        Mockito.verify(userServiceMock, times(1)).getUserByEmail("lucii@gmail");
+    }
+
+    @Test
+    void getUsersIT() throws Exception{
         //Given
         List<GetUserDto> getUserDtoList = new ArrayList<>();
         GetUserDto getUserDto = new GetUserDto();
@@ -109,7 +104,7 @@ class UserControllerTest {
                 "45464546", true, "", LocalDate.now(), roles);
 
         //When
-        when(userServiceMock.createUser(postUserDto)).thenReturn(getUserDto);
+        Mockito.when(userServiceMock.createUser(postUserDto)).thenReturn(getUserDto);
 
         //Then
         mockMvc.perform(MockMvcRequestBuilders.post("/users")
@@ -120,5 +115,109 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.name").value(postUserDto.getName()));
 
         Mockito.verify(userServiceMock, times(1)).createUser(Mockito.any(PostUserDto.class));
+    }
+
+    @Test
+    void getUserByIdIT() throws Exception{
+        //Given
+        String[] roles = {"SuperAdmin"};
+        GetUserDto getUserDto = new GetUserDto(1, "Lucía", "Fernanda", "Lucifer", "123456", "lucii@gmail", "3515623",
+                "4523545", true, "", LocalDate.now(), roles);
+
+        //When
+        Mockito.when(userServiceMock.getUserById(1)).thenReturn(getUserDto);
+
+        //Then
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/get/1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("Lucía"));
+
+        Mockito.verify(userServiceMock, times(1)).getUserById(Mockito.anyInt());
+    }
+
+    @Test
+    void getUsersByStatusIT() throws Exception{
+        //Given
+        List<GetUserDto> getUserDtoList = new ArrayList<>();
+        GetUserDto getUserDto = new GetUserDto(1, "Lucía", "Fernanda", "Lucifer", "123456", "lucii@gmail", "3515623",
+                "4523545", true, "", LocalDate.now(), new String[]{"SuperAdmin"});
+
+        getUserDtoList.add(getUserDto);
+        getUserDtoList.add(getUserDto);
+        getUserDtoList.add(getUserDto);
+
+        //When
+        Mockito.when(userServiceMock.getUsersByStatus(true)).thenReturn(getUserDtoList);
+
+        //Then
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/status")
+                        .param("isActive", "true")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(3));
+
+        Mockito.verify(userServiceMock, times(1)).getUsersByStatus(true);
+    }
+
+    @Test
+    void getUsersByRoleIT() throws Exception{
+        //Given
+        List<GetUserDto> getUserDtoList = new ArrayList<>();
+        GetUserDto getUserDto = new GetUserDto(1, "Lucía", "Fernanda", "Lucifer", "123456", "lucii@gmail", "3515623",
+                "4523545", true, "", LocalDate.now(), new String[]{"Admin"});
+
+        getUserDtoList.add(getUserDto);
+
+        //When
+        Mockito.when(userServiceMock.getUsersByRole(1)).thenReturn(getUserDtoList);
+
+        //Then
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/role")
+                        .param("roleId", "1")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(1))
+                .andExpect(jsonPath("$[0].roles[0]").value("Admin"));
+
+        Mockito.verify(userServiceMock, times(1)).getUsersByRole(1);
+    }
+
+    @Test
+    void deleteUserIT() throws Exception{
+        //When
+        Mockito.doNothing().when(userServiceMock).deleteUser(1);
+
+        //Then
+        mockMvc.perform(MockMvcRequestBuilders.delete("/users/1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        Mockito.verify(userServiceMock, times(1)).deleteUser(1);
+    }
+
+    @Test
+    void updateUserIT() throws Exception{
+        //Given
+        PutUserDto putUserDto = new PutUserDto(1, "Lucía", "Fernanda", "4523545", "3515623",
+                "lucii@gmail", "", LocalDate.now(), Arrays.asList(2));
+
+        GetUserDto getUserDto = new GetUserDto(1, "Lucía", "Fernanda", "Lucifer", "123456",
+                "lucii@gmail", "1111111", "4523545", true, "", LocalDate.now(), new String[]{"Security"});
+
+        //When
+        Mockito.when(userServiceMock.updateUser(putUserDto)).thenReturn(getUserDto);
+
+        //Then
+        mockMvc.perform(MockMvcRequestBuilders.put("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapperMock.writeValueAsString(putUserDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.phone_number").value("1111111"))
+                .andExpect(jsonPath("$.roles[0]").value("Security"));
+
+        Mockito.verify(userServiceMock, times(1)).updateUser(putUserDto);
     }
 }
