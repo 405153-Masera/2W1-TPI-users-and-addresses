a280e9cb4426539fe4833a327050f2927edc7804
405786MoroBenjamin <405786@tecnicatura.frc.utn.edu.ar>
2024-10-15 00:08:12 -0300

    Update CorsConfig.java

--- src/main/java/ar/edu/utn/frc/tup/lc/iv/configs/CorsConfig.java
+++ src/main/java/ar/edu/utn/frc/tup/lc/iv/configs/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
 }
