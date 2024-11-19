51de67bb40db05d985d1f6b84f6f73fa463a2df8
405153-Masera <405153@tecnicatura.frc.utn.edu.ar>
2024-11-19 00:14:21 -0300

    Test rest acces y rest contact

--- src/test/java/ar/edu/utn/frc/tup/lc/iv/restTemplate/RestContactTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/restTemplate/RestContactTest.java
@@ -1,14 +1,16 @@
 package ar.edu.utn.frc.tup.lc.iv.restTemplate;
 
+import ar.edu.utn.frc.tup.lc.iv.restTemplate.contacts.ContactPutRequest;
 import ar.edu.utn.frc.tup.lc.iv.restTemplate.contacts.ContactRequest;
 import ar.edu.utn.frc.tup.lc.iv.restTemplate.contacts.GetContactDto;
 import ar.edu.utn.frc.tup.lc.iv.restTemplate.contacts.RestContact;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -20,85 +22,85 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class RestContactTest {
 
-    @MockBean
+    @Mock
     private RestTemplate restTemplate;
 
-    @SpyBean
+    @InjectMocks
     private RestContact restContact;
 
-//    @Test
-//    void getContactById() throws Exception {
-//
-//        String jsonResponse = "[{\"contactType\":{\"id\":\"1\"},\"value\":\"email@email.com\"}]";
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
-//
-//        when(restTemplate.getForEntity(anyString(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(jsonNode, HttpStatus.OK));
-//
-//        List<GetContactDto> contacts = restContact.getContactById(2);
-//
-//        assertNotNull(contacts);
-//        assertEquals(1, contacts.get(0).getType_contact());
-//        assertEquals("email@email.com", contacts.get(0).getValue());
-//    }
+    @Test
+    void getContactById() throws Exception {
 
-//    @Test
-//    void getAllEmails() throws Exception {
-//        String jsonResponse = "[{\"contactType\":{\"id\":\"1\"},\"value\":\"email@email.com\"}," +
-//                "{\"contactType\":{\"id\":\"2\"},\"value\":\"34354353\"}]";
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
-//
-//        when(restTemplate.getForEntity(anyString(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(jsonNode, HttpStatus.OK));
-//
-//        List<String> emails = restContact.getAllEmails();
-//
-//        assertNotNull(emails);
-//        assertTrue(emails.contains("email@email.com"));
-//    }
+        String jsonResponse = "[{\"contactType\":{\"id\":\"1\"},\"value\":\"email@email.com\"}]";
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
 
-//    @Test
-//    void getUserIdByEmail() throws Exception {
-//        String email = "email@email.com";
-//        String jsonResponse = "[{\"userId\":20,\"contactType\":{\"id\":\"1\"},\"value\":\"email@email.com\"}," +
-//                "{\"userId\":2,\"contactType\":{\"id\":\"1\"},\"value\":\"email@email2.com\"}]";
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
-//
-//        when(restTemplate.getForEntity(anyString(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(jsonNode, HttpStatus.OK));
-//
-//        Integer userId = restContact.getUserIdByEmail(email);
-//
-//        assertNotNull(userId);
-//        assertEquals(20, userId);
-//    }
+        when(restTemplate.getForEntity(anyString(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(jsonNode, HttpStatus.OK));
 
-//    @Test
-//    void saveContact() {
-//        Integer userId = 1;
-//        String value = "email@email.com";
-//        int contactType = 1;
-//
-//        when(restTemplate.postForEntity(anyString(), any(ContactRequest.class), eq(Void.class)))
-//                .thenReturn(new ResponseEntity<>(HttpStatus.OK));
-//
-//        boolean result = restContact.saveContact(userId, value, contactType,1);
-//
-//        assertTrue(result);
-//    }
+        List<GetContactDto> contacts = restContact.getContactById(2);
 
-//    @Test
-//    void updateContact() {
-//        Integer userId = 1;
-//        String value = "email@email.com";
-//        int contactType = 1;
-//
-//        doNothing().when(restTemplate).put(anyString(), any(ContactRequest.class));
-//
-//        boolean result = restContact.updateContact(userId, value, contactType,1);
-//        assertTrue(result);
-//    }
+        assertNotNull(contacts);
+        assertEquals(1, contacts.get(0).getType_contact());
+        assertEquals("email@email.com", contacts.get(0).getValue());
+    }
+
+    @Test
+    void getAllEmails() throws Exception {
+        String jsonResponse = "[{\"contactType\":{\"id\":\"1\"},\"value\":\"email@email.com\"}," +
+                "{\"contactType\":{\"id\":\"2\"},\"value\":\"34354353\"}]";
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+
+        when(restTemplate.getForEntity(anyString(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(jsonNode, HttpStatus.OK));
+
+        List<String> emails = restContact.getAllEmails();
+
+        assertNotNull(emails);
+        assertTrue(emails.contains("email@email.com"));
+    }
+
+    @Test
+    void getUserIdByEmail() throws Exception {
+        String email = "email@email.com";
+        String jsonResponse = "[{\"userId\":20,\"contactType\":{\"id\":\"1\"},\"value\":\"email@email.com\"}," +
+                "{\"userId\":2,\"contactType\":{\"id\":\"1\"},\"value\":\"email@email2.com\"}]";
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+
+        when(restTemplate.getForEntity(anyString(), eq(JsonNode.class))).thenReturn(new ResponseEntity<>(jsonNode, HttpStatus.OK));
+
+        Integer userId = restContact.getUserIdByEmail(email);
+
+        assertNotNull(userId);
+        assertEquals(20, userId);
+    }
+
+    @Test
+    void saveContact() {
+        Integer userId = 1;
+        String value = "email@email.com";
+        int contactType = 1;
+
+        when(restTemplate.postForEntity(anyString(), any(ContactRequest.class), eq(Void.class)))
+                .thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+        boolean result = restContact.saveContact(userId, value, contactType,1);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void updateContact() {
+        Integer userId = 1;
+        String value = "email@email.com";
+        int contactType = 1;
+
+        doNothing().when(restTemplate).put(anyString(), any(ContactPutRequest.class));
+
+        boolean result = restContact.updateContact(userId, value, contactType,1);
+        assertTrue(result);
+    }
 }
--- src/test/java/ar/edu/utn/frc/tup/lc/iv/restTemplate/access/RestAccessTest.java
+++ src/test/java/ar/edu/utn/frc/tup/lc/iv/restTemplate/access/RestAccessTest.java
@@ -4,90 +4,93 @@ import ar.edu.utn.frc.tup.lc.iv.dtos.post.BasePostUser;
 import ar.edu.utn.frc.tup.lc.iv.dtos.post.PostOwnerUserDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class RestAccessTest {
 
-    @MockBean
+    @Mock
     private RestTemplate restTemplate;
 
-    @SpyBean
+    @InjectMocks
     private RestAccess restAccess;
 
-
-    private static final String BASE_URL = "http://localhost:8090/owner-tenant";
+    private static final String BASE_URL = "http:localhost:8090/owner-tenant";
 
     @BeforeEach
     void setUp() {
         restAccess.setUrl(BASE_URL);
     }
 
-//    @Test
-//    void postAccess() {
-//        List<AccessPost> accessPosts = new ArrayList<>();
-//        AccessPost accessPost = new AccessPost();
-//        accessPost.setDocument("12345678");
-//        accessPost.setName("John");
-//        accessPost.setLast_name("Doe");
-//        accessPosts.add(accessPost);
-//
-//        when(restTemplate.postForEntity(
-//                eq(BASE_URL + "/register"),
-//                eq(accessPosts),
-//                eq(Void.class)))
-//                .thenReturn(ResponseEntity.ok().build());
-//
-//        restAccess.postAccess(accessPosts);
-//        verify(restTemplate).postForEntity(
-//                eq(BASE_URL + "/register"),
-//                eq(accessPosts),
-//                eq(Void.class)
-//        );
-//    }
+    @Test
+    void postAccess() {
+        List<AccessPost> accessPosts = new ArrayList<>();
+        AccessPost accessPost = new AccessPost();
+        accessPost.setDocument("12345678");
+        accessPost.setName("John");
+        accessPost.setLast_name("Doe");
+        accessPosts.add(accessPost);
 
-//    @Test
-//    void deleteAccess() {
-//        String document = "12345678";
-//        restAccess.deleteAccess(document);
-//        verify(restTemplate).put("http://localhost:8090/owner-tenant/unsubscribe/12345678", null, Void.class);
-//    }
+        when(restTemplate.postForEntity(
+                eq(BASE_URL + "/owner_tenant/register"),
+                eq(accessPosts),
+                eq(Void.class)))
+                .thenReturn(ResponseEntity.ok().build());
 
-//    @Test
-//    void registerUserAccess() {
-//        BasePostUser user = createBasePostUser("12345678", "John", "Doe", "john@example.com", new String[]{"Propietario"});
-//        ArgumentCaptor<List<AccessPost>> accessPostCaptor = ArgumentCaptor.forClass(List.class);
-//
-//        restAccess.registerUserAccess(user);
-//        verify(restTemplate).postForEntity(
-//                eq(BASE_URL + "/register"),
-//                accessPostCaptor.capture(),
-//                eq(Void.class)
-//        );
-//
-//        List<AccessPost> capturedAccessPosts = accessPostCaptor.getValue();
-//        assertEquals(1, capturedAccessPosts.size());
-//        AccessPost capturedPost = capturedAccessPosts.get(0);
-//
-//        assertEquals("12345678", capturedPost.getDocument());
-//        assertEquals("John", capturedPost.getName());
-//        assertEquals("Doe", capturedPost.getLast_name());
-//        assertEquals("DNI", capturedPost.getDocumentType().getDescription());
-//        assertEquals("Propietario", capturedPost.getUser_allowed_Type().getDescription());
-//        assertEquals("john@example.com", capturedPost.getEmail());
-//    }
+        restAccess.postAccess(accessPosts);
+        verify(restTemplate).postForEntity(
+                eq(BASE_URL + "/owner_tenant/register"),
+                eq(accessPosts),
+                eq(Void.class)
+        );
+    }
+
+    @Test
+    void deleteAccess() {
+        String document = "12345678";
+        restAccess.deleteAccess(document);
+        verify(restTemplate).put(
+                eq(BASE_URL + "/owner_tenant/unsubscribe/" + document),
+                isNull(),
+                eq(Void.class)
+        );
+    }
+
+    @Test
+    void registerUserAccess() {
+        BasePostUser user = createBasePostUser("12345678", "John", "Doe", "john@example.com", new String[]{"Propietario"});
+        ArgumentCaptor<List<AccessPost>> accessPostCaptor = ArgumentCaptor.forClass(List.class);
+
+        restAccess.registerUserAccess(user);
+        verify(restTemplate).postForEntity(
+                eq(BASE_URL + "/owner_tenant/register"),
+                accessPostCaptor.capture(),
+                eq(Void.class)
+        );
+
+        List<AccessPost> capturedAccessPosts = accessPostCaptor.getValue();
+        assertEquals(1, capturedAccessPosts.size());
+        AccessPost capturedPost = capturedAccessPosts.get(0);
+
+        assertEquals("12345678", capturedPost.getDocument());
+        assertEquals("John", capturedPost.getName());
+        assertEquals("Doe", capturedPost.getLast_name());
+        assertEquals("DNI", capturedPost.getDocumentType().getDescription());
+        assertEquals("Owner", capturedPost.getUser_allowed_Type().getDescription());
+        assertEquals("john@example.com", capturedPost.getEmail());
+    }
 
     private BasePostUser createBasePostUser(String dni, String name, String lastname, String email, String[] roles) {
         BasePostUser user = new PostOwnerUserDto();
